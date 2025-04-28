14ccacc946836c38435f9920c74bc4d8baf07623
kamoliddin <kamoliddinzaylobiddinov677@gmail.com>
2025-04-28 09:07:53 +0500

    returning complex object from Rest API

--- KudVenKat_Asp.Net_Web_API/Controllers/StudentController.cs
+++ KudVenKat_Asp.Net_Web_API/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KudVenKat_Asp.Net_Web_API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KudVenKat_Asp.Net_Web_API.Controllers
 {
@@ -7,9 +8,26 @@ namespace KudVenKat_Asp.Net_Web_API.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
-        public string GetStudents()
+        public IEnumerable<Student> GetStudents()
         {
-            return "All students";
+            return new List<Student>
+            {
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Ali",
+                    Email = "Example@gmail.com",
+                    Address = "Tashkent"
+                },
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Ali",
+                    Email = "Example@gmail.com",
+                    Address = "Tashkent"
+                }
+
+            };
         }
     }
 }
---
+++ KudVenKat_Asp.Net_Web_API/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace KudVenKat_Asp.Net_Web_API.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+    }
+}
