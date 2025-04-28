baa471999287b193e010987dceed0dd6e863b813
kamoliddin <kamoliddinzaylobiddinov677@gmail.com>
2025-04-28 08:53:31 +0500

    first web api project

---
+++ KudVenKat_Asp.Net_Web_API/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KudVenKat_Asp.Net_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "All students";
+        }
+    }
+}
